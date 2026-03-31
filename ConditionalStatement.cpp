8afe69732fb7d41132bb97090979da93e13bd4af
RazzanIkmalSaputra <razzanikmalsaputra9@gmail.com>
2026-03-31 12:06:41 +0700

    Menambahkan kode untuk menampilkan hasil akhir ke layar sesuai format

--- ConditionalStatement.cpp
+++ ConditionalStatement.cpp
@@ -1,19 +1,7 @@
 
 
-int main() {
-    double berat, tinggi, bmi;
-
-  
-    masukanData(berat, tinggi);
-
-
-    bmi = hitungBMI(berat, tinggi);
-
-
     cout << endl;
     cout << "--- Hasil ---" << endl;
     cout << "BMI Anda : " << bmi << endl;
     cout << "Status   : " << cekKondisi(bmi) << endl;
 
-    return 0;
-}       
