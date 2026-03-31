e51713b45484f603d9b114fd61bbcb32ef4712b1
RazzanIkmalSaputra <razzanikmalsaputra9@gmail.com>
2026-03-31 12:05:04 +0700

    Menambahkan fungsi matematis untuk menghitung nilai BMI

--- ConditionalStatement.cpp
+++ ConditionalStatement.cpp
@@ -1,10 +1,5 @@
 
-
-
-void masukanData(double &berat, double &tinggi) {
-    cout << "--- Selamat Datang Mahasiswa Kelas F 2025 ---" << endl;
-    cout << "Masukkan berat badan (kg): ";
-    cin >> berat;
-    cout << "Masukkan tinggi badan (meter): ";
-    cin >> tinggi;
+double hitungBMI(double berat, double tinggi) {
+    return berat / (tinggi * tinggi);
 }
+
